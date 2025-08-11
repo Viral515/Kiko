bae7c9905950883e9b55e0683e94658745cfb236
Viral515 <nikish515@bk.ru>
2025-08-12 06:28:53 +0700

    Переделал запуск игр стима, теперь достаточно указать айди игры стим в описании команды, вместо внесения её в список game/id в скрипте.

--- src/scripts/steam/launch_game.ps1
+++ src/scripts/steam/launch_game.ps1
@@ -1,31 +1,7 @@
 param(
-    [string]$GameName,
+    [string]$AppId,
     [string]$SteamPath = "C:\Program Files (x86)\Steam\steam.exe"
 )
-
-# TODO: вынести список игр в отдельный файл
-$games = @{
-    "dota" = "570"
-    "дота" = "570"
-    "dota 2" = "570"
-    "дота 2" = "570"
-    "portal 2" = "620"
-    "портал 2" = "620"
-    "nightraign" = "2622380"
-}
-
-function Find-Game {
-    param([string]$Name)
-    
-    $normalizedName = $Name.ToLower().Trim()
-    
-    foreach ($game in $games.GetEnumerator()) {
-        if ($game.Key -like "*$normalizedName*" -or $normalizedName -like "*$($game.Key)*") {
-            return $game.Value
-        }
-    }
-    return $null
-}
 
 function Launch-SteamGame {
     param([string]$AppId)
@@ -45,22 +21,4 @@ function Launch-SteamGame {
     }
 }
 
-if ($GameName) {
-    $appId = Find-Game -Name $GameName
-    
-    if ($appId) {
-        $success = Launch-SteamGame -AppId $appId
-        if ($success) {
-            Write-Host "Game '$GameName' is launching..."
-        }
-    } else {
-        Write-Host "Game '$GameName' not found in list"
-        Write-Host "Available games:"
-        $games.GetEnumerator() | ForEach-Object {
-            Write-Host "  - $($_.Key)"
-        }
-    }
-} else {
-    Write-Host "No game name specified"
-    Write-Host "Usage: .\launch_game.ps1 -GameName 'game name'"
-}
+Launch-SteamGame -AppId $AppId
