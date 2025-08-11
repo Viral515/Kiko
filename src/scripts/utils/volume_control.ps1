d06b31a022e8996bfce615aa5ea1de195a5222b1
Viral515 <nikish515@bk.ru>
2025-08-12 05:45:29 +0700

    Подправил команды и их распознавание (добавил больше параметров в нормализацию). Пока убрал команды для работы с дискордом.

--- src/scripts/utils/volume_control.ps1
+++ src/scripts/utils/volume_control.ps1
@@ -1,9 +1,7 @@
-# Управление громкостью системы
 param(
     [string]$Action
 )
 
-# Функция для отправки горячих клавиш
 function Send-VolumeKey {
     param([string]$Key)
     
@@ -12,24 +10,9 @@ function Send-VolumeKey {
     Start-Sleep -Milliseconds 100
 }
 
-# Основная логика
 if ($Action -eq "mute") {
     Send-VolumeKey([char]173)
-    Write-Host "Звук системы переключен"
 }
 elseif ($Action -eq "unmute") {
     Send-VolumeKey([char]173)
-    Write-Host "Звук системы переключен"
-}
-elseif ($Action -eq "up") {
-    Send-VolumeKey([char]175)
-    Write-Host "Громкость увеличена"
-}
-elseif ($Action -eq "down") {
-    Send-VolumeKey([char]174)
-    Write-Host "Громкость уменьшена"
-}
-else {
-    Write-Host "Неизвестное действие: $Action"
-    Write-Host "Доступные действия: mute, unmute, up, down"
 }
